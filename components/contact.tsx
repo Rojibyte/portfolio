9893f91f1620e44f1e3baead1a18aab759859914
rojiro <fd.rogeliosantiago@gmail.com>
2024-07-02 01:33:38 +0800

    Finished Contact Section: Learned advanced error handling using try catch and code cleaning/refactor Learned how to send email using Resend Learned how to style email using React.Email Added in bootstrap like notifications when getting error messages using react-hot-toast Added in footer with styling

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,14 +1,32 @@
 "use client";
 import React from "react";
 import SectionHeading from "./section-heading";
-import { FaPaperPlane } from "react-icons/fa";
 import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "./submit-btn";
+import toast from "react-hot-toast";
 
 export default function Contact() {
+  const { ref } = useSectionInView("Contact");
+
   return (
     <motion.section
       id="contact"
+      ref={ref}
       className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
     >
       <SectionHeading>Contact Me</SectionHeading>
 
@@ -20,27 +38,36 @@ export default function Contact() {
         or through this form.
       </p>
 
-      <form className="mt-10 flex flex-col">
+      <form
+        className="mt-10 flex flex-col"
+        action={async (formData) => {
+          const { data, error } = await sendEmail(formData);
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success("Email sent successfully!");
+        }}
+      >
         <input
           className="h-14 px-4 rounded-lg borderBlack"
+          name="senderEmail"
           type="email"
           placeholder="Your email"
-          name=""
+          required
+          maxLength={500}
           id=""
         />
         <textarea
           className="h-52 my-3 rounded-lg borderBlack p-4"
+          name="message"
           placeholder="Your message"
-          name=""
+          required
+          maxLength={5000}
           id=""
         />
-        <button
-          type="submit"
-          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition-all"
-        >
-          Submit{" "}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   );
